3df14020d879c71d96648fbe254dcbd8877c7839
BriChavez <brianachavez@gmail.com>
2022-05-20 14:44:50 -0700

    clean up the code into the main.py, reworked the syntax of functions to better fit the csv

--- main.py
+++ main.py
@@ -2,34 +2,63 @@
 import pandas as pd
 
 # import pandas and set pandas read_cvs to a variable
-ufo_data =pd.read_csv('./data/nuforc_reports.csv')
-# ufo_data.head()
+ufo_filepath = './data/nuforc_reports.csv'
+ufo_data = pd.read_csv(ufo_filepath)
 df = ufo_data
-df.shape
+# df.head()
+
+# # create empty variables so we know what internal variablesneed to define later
+# col_list = []
+# col_check = []
+
 
 """start functions to clean data"""
-def fill_na(df):
-    """check to see if there are any null values and changes them to NaN"""
-    df.fillna(value = "null", axis = 1, inplace = True)
 
 
-def drop_dupes(dataframe, col_check=None):
-    """drop duplicates among the summaries to ensure the data isnt repeated"""
-    df.drop_duplicates(subset=[col_check], inplace=True)
-
-
-# if our data doesnt already have one, this is a function to create an index from a later specified column
-"""create the index"""
-
-def add_index(self, col_list, index_name="index"):
+"""if our data doesnt already have one, this is a function to create an index from a later specified column"""
+def set_index(self, index=None):
     df = self.df
     """create the index by concatting two columns"""
-    df[index_name] = df[col_list].apply(lambda row: "-".join(row.values.astype(str)), axis=1)
-    df = df.set_index(index_name, inplace=True)
+    # df[index_name] = df[col_list]
+    # .apply(
+    # lambda row: "-".join(row.values.astype(str)), axis=1)
+    df = df.set_index(index, inplace=True)
     self.df = df
 
 
+def fill_na(df):
+    """check to see if there are any null values and changes them to NaN"""
+    df.fillna(value="null", axis=1, inplace=True)
+
+
+def drop_dupes(df, col_check=None):
+    """drop duplicates among the specified col to ensure the data isnt repeated"""
+    df.drop_duplicates(subset=[col_check], inplace=True)
+
+
 """write the pandas df back to a csv"""
+
+
 def write_csv(df):
-    clean_UFO_data = (f'./data/cleaned_UFO_data.csv')
+    clean_UFO_data = './data/cleaned_UFO_data.csv'
     df.to_csv(clean_UFO_data)
+
+
+"""run function to enact our functions on our ufo dataframe"""
+# df = ufo_data
+# col_check = df.summary
+
+# df['date_time'] = pd.to_datetime(df['date_time'], format="%Y-%m-%d")
+# col_list = [df.date_time]
+
+
+def run(df):
+    set_index(df, index)
+    fill_na(df)
+    drop_dupes(df, ['col_check'])
+    write_csv(df)
+
+
+run(ufo_data)
+
+# print(df.head())
