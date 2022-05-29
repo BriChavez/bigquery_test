6d4265a49dfcd1c7f0c46ee839e871c77de27bd8
BriChavez <brianachavez@gmail.com>
2022-05-29 01:18:09 -0700

    retype the code to set for a new cvs to use. wip

--- main.py
+++ main.py
@@ -3,47 +3,40 @@ import pandas as pd
 from pandas import DataFrame
 
 # import pandas and set pandas read_cvs to a variable
-ufo_data = pd.read_csv('./data/nuforc_reports.csv', index_col=0)
-df = pd.DataFrame(ufo_data, columns=ufo_data.iloc[1])
+ufo_data = pd.read_csv('./data/complete.csv')
+df = pd.DataFrame(ufo_data)
 # df.head()
 
 
 """start functions to clean data"""
 
-
-def set_header(df):
-    header_row = df.iloc[1]
-    df = df.values[1:]
-
+# def set_header(df):
+#     header_row = df.iloc[1]
+#     df = df.values[1:]
+    
 
 def set_index(df):
     """if our data doesnt already have one, this is a function to create an index"""
-    df = df.set_index('stats')
+    df = df.set_index('datetime')
 
 
 def fill_na(df):
     """check to see if there are any null values and changes them to NaN"""
-    df.fillna(value="null", axis=1, inplace=True)
-
-
-"""write the pandas df back to a csv"""
+    df.fillna(value="null", inplace=True)
 
 
 def write_csv(df):
+    """write the pandas df back to a csv"""
     clean_UFO_data = './data/cleaned_UFO_data.csv'
     df.to_csv(clean_UFO_data)
 
-
-"""write the run function and call it on out dataframe"""
-
-
 def run(df):
-    set_header(df)
+    """write the run function and call it on out dataframe"""
+    # set_header(df)
     set_index(df)
     fill_na(df)
     write_csv(df)
 
 
 run(ufo_data)
-
-# print(df.head())
+"""call run function on our database"""
