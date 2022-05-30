2632b8413c0eac75b916a1e9e820d35ce588e1ec
BriChavez <brianachavez@gmail.com>
2022-05-30 13:36:59 -0700

    add code to clean up date_time as an index and ensure none of the entries are missing datetime

--- main.py
+++ main.py
@@ -21,27 +21,35 @@ df = pd.DataFrame(ufo_data)
 """start functions to help clean the dataframe"""
 
 def drop_cols(df):
-    """drop columns that we dont want to play with to make the file size smaller"""
-    df.drop(columns = ['report_link', 'posted', 'city_latitude', 'city_longitude', 'stats', 'text'], inplace = True)
+    """drop columns"""
+    # drop columns in the dataframe we dont want to play with to make the file size smaller
+    df.drop(columns = ['report_link', 'posted', 'city_latitude', 'city_longitude', 'stats'], inplace = True)
 
 def fill_na(df):
-    """check to see if there are any null values and changes them to NaN"""
-    df.fillna(value="N/A", inplace=True)
+    """fill empty/na values"""
+    # check to see if there are any null values and changes them to a string
+    df.fillna(value='null', inplace=True)
 
 def drop_na(df):
     """drop rows where information is missing"""
     # drop entire row where the feild date_time is missing
-    df.dropna(subset = ['date_time'], how = 'all', inplace = True)
+    df.dropna(subset=['date_time'], inplace=True)
     # drop entire rows where there is less than 4 columns with values
     df.dropna(thresh =4, inplace= True)
 
+def clean_index(df):
+    """drop rows where the index is set to  an n/a string"""
+    # use  drop to delete the rows where the date_time feild shows as our missing data string
+    df.drop(index = ['null'], inplace = True)
 
 def set_index(df):
-    """if our data doesnt already have one, this is a function to create an index"""
+    """set the index"""
+    # use set_index to use the column date_time to be our index
     df = df.set_index(['date_time'], inplace=True)
 
 def write_csv(df):
     """write the pandas df back to a csv"""
+    # function to create a new csv file and write the cleaned up data to it
     clean_UFO_data = './data/cleaned_UFO_data.csv'
     df.to_csv(clean_UFO_data)
 
@@ -50,7 +58,8 @@ def run(df):
     drop_cols(df)
     fill_na(df)
     drop_na(df)
-    set_index(df)   
+    set_index(df)
+    clean_index(df)
     write_csv(df)
 
 
