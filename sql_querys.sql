b1ca1357559798251b95f5a7008dd54f65e09daf
BriChavez <brianachavez@gmail.com>
2022-05-31 02:08:44 -0700

    add three more sql queries hitting all the specified clauses

--- sql_querys.sql
+++ sql_querys.sql
@@ -22,5 +22,22 @@ WHERE text LIKE '%chill%'
 -- returns 346
 
 
+"""ask what the last date recorded is"""
+SELECT MAX(date_time)
+FROM `ughh-347120.ufo_dataset.ufo_table` 
+-- returns 2019-12-29
 
 
+"""ask if any occurances have happened in guatemala"""
+SELECT *
+FROM `ughh-347120.ufo_dataset.ufo_table` 
+WHERE city IN ("Guatemala")
+-- returns that two have. 
+
+
+"""ask what shapes have been reported in Portland"""
+SELECT city, shape
+FROM `ughh-347120.ufo_dataset.ufo_table` 
+WHERE city LIKE '%Portland%'
+ORDER BY shape
+-- returns changing, cigar, chevron, and circle
